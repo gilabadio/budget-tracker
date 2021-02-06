99e9d2f3f94b54c8f7d4940a68095d5693e55631
G.B.Abadio <gilabadio@gmail.com>
2021-02-05 22:43:39 -0500

    still with persistent bug tring out this now on heroky

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-
-
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
@@ -12,8 +10,6 @@ router.post("/api/transaction", ({body}, res) => {
       res.status(404).json(err);
     });
 });
-
-
 
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
@@ -25,8 +21,6 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-
-
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
@@ -36,7 +30,5 @@ router.get("/api/transaction", (req, res) => {
       res.status(404).json(err);
     });
 });
-
-
 
 module.exports = router;
